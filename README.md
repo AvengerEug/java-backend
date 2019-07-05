5bc5b5d18b9753622bac536abc995ceb2f4c9032
Eugene.Huang <eugenehuang@augmentum.com.cn>
2019-07-05 17:43:17 +0800

    init java backend group project

---
+++ README.md
@@ -0,0 +1,19 @@
+# java-backend
+java技术后端group root项目
+
+## 使用步骤
+
+1. clone develop分支至本地
+   ```
+      git clone https://github.com/EugeneHuang9638/java-backend.git -b develop
+   ```
+   
+2. 执行如下脚本clone所有项目(develop分支)
+   ```shell
+     BRANCH=develop ./build.sh clone
+   ```
+
+3. 使用idea或者eclipse等其他编辑器以maven project的形式导入
+
+4. 因为其中的某些project会依赖与lib-common项目, 所以在拉取项目后需要在java-backend路径下执行mvn clean install命令
+
---
+++ build.sh
@@ -0,0 +1,90 @@
+#!/bin/bash -e
+
+opts="clone"
+
+BRANCH=${BRANCH:-"master"}
+
+clone() {
+  echo "=============================================="
+  echo "Clone project with branch $BRANCH"
+  echo "=============================================="
+
+  clone_javase
+  clone_lib-common
+  clone_dynamic-proxy
+  clone_dynamic-proxy-adapter
+  clone_simple-factory
+  clone_jwt-token
+
+  echo "=============================================="
+  echo "Clone project done."
+  echo "=============================================="
+}
+
+clone_javase() {
+  echo -e "\nClone javase"
+  echo "----------------------------------------------"
+  if [ ! -e ./basic/javase ]; then
+    git clone https://github.com/EugeneHuang9638/javase.git -b $BRANCH ./basic/javase
+  fi
+}
+
+clone_lib-common() {
+  echo -e "\nClone lib-common"
+  echo "----------------------------------------------"
+  if [ ! -e ./lib/lib-common ]; then
+    git clone https://github.com/EugeneHuang9638/lib-common.git -b $BRANCH ./lib/lib-common
+  fi
+}
+
+clone_dynamic-proxy() {
+  echo -e "\nClone dynamic-proxy"
+  echo "----------------------------------------------"
+  if [ ! -e ./design/dynamic-proxy ]; then
+    git clone https://github.com/EugeneHuang9638/dynamic-proxy.git -b $BRANCH ./design/dynamic-proxy
+  fi
+}
+
+clone_dynamic-proxy-adapter() {
+  echo -e "\nClone dynamic-proxy-adapter"
+  echo "----------------------------------------------"
+  if [ ! -e ./design/dynamic-proxy-adapter ]; then
+    git clone https://github.com/EugeneHuang9638/dynamic-proxy-adapter.git -b $BRANCH ./design/dynamic-proxy-adapter
+  fi
+}
+
+clone_simple-factory() {
+  echo -e "\nClone simple-factory"
+  echo "----------------------------------------------"
+  if [ ! -e ./design/simple-factory ]; then
+    git clone https://github.com/EugeneHuang9638/simple-factory.git -b $BRANCH ./design/simple-factory
+  fi
+}
+
+clone_jwt-token() {
+  echo -e "\nClone jwt-token"
+  echo "----------------------------------------------"
+  if [ ! -e ./javaee/jwt-token ]; then
+    git clone https://github.com/EugeneHuang9638/jwt-token.git -b $BRANCH ./javaee/jwt-token
+  fi
+}
+
+usage() {
+  echo "USAGE: $0" option key
+
+  echo -e "\nOptions:"
+  for opt in $opts; do
+    echo "    ${opt}"
+  done
+  echo -e ""
+}
+
+
+case "$1" in
+  clone)
+    clone
+    ;;
+  *)
+    usage
+    ;;
+esac
