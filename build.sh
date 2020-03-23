c4d9fa7189fe9a20edf9b9ef81963f393a48a9f3
AvengerEug <eugenesumarry@163.com>
2020-03-23 10:12:26 +0800

    新增jenkins-for-dynamic-proxy分支, 集成jekins

--- build.sh
+++ build.sh
@@ -9,56 +9,14 @@ clone() {
   echo "Clone project with branch $BRANCH"
   echo "=============================================="
 
-  clone_javase
   clone_lib-common
-  clone_dynamic-proxy
   clone_dynamic-proxy-adapter
-  clone_template-method
-  clone_simple-factory
-  clone_observer
-  clone_jwt-token
-  clone_rabbit-mq
-  clone_spring
-  clone_springboot-study
-  clone_spring-cloud
-  clone_distributed
 
   echo "=============================================="
   echo "Clone project done."
   echo "=============================================="
 }
 
-clone_distributed() {
-  echo -e "\nClone distributed"
-  echo "----------------------------------------------"
-  if [ ! -e ./basic/distributed ]; then
-    git clone https://github.com/AvengerEug/distributed.git -b $BRANCH ./basic/distributed
-  fi
-}
-
-clone_spring-cloud() {
-  echo -e "\nClone spring-cloud"
-  echo "----------------------------------------------"
-  if [ ! -e ./basic/spring-cloud ]; then
-    git clone https://github.com/AvengerEug/spring-cloud.git -b $BRANCH ./basic/spring-cloud
-  fi
-}
-
-clone_springboot-study() {
-  echo -e "\nClone springboot-study"
-  echo "----------------------------------------------"
-  if [ ! -e ./basic/springboot-study ]; then
-    git clone https://github.com/AvengerEug/springboot-study.git -b $BRANCH ./basic/springboot-study
-  fi
-}
-
-clone_javase() {
-  echo -e "\nClone javase"
-  echo "----------------------------------------------"
-  if [ ! -e ./basic/javase ]; then
-    git clone https://github.com/AvengerEug/javase.git -b $BRANCH ./basic/javase
-  fi
-}
 
 clone_lib-common() {
   echo -e "\nClone lib-common"
@@ -68,67 +26,11 @@ clone_lib-common() {
   fi
 }
 
-clone_dynamic-proxy() {
-  echo -e "\nClone dynamic-proxy"
-  echo "----------------------------------------------"
-  if [ ! -e ./design/dynamic-proxy ]; then
-    git clone https://github.com/AvengerEug/dynamic-proxy.git -b $BRANCH ./design/dynamic-proxy
-  fi
-}
-
 clone_dynamic-proxy-adapter() {
   echo -e "\nClone dynamic-proxy-adapter"
   echo "----------------------------------------------"
   if [ ! -e ./design/dynamic-proxy-adapter ]; then
     git clone https://github.com/AvengerEug/dynamic-proxy-adapter.git -b $BRANCH ./design/dynamic-proxy-adapter
-  fi
-}
-
-clone_simple-factory() {
-  echo -e "\nClone simple-factory"
-  echo "----------------------------------------------"
-  if [ ! -e ./design/simple-factory ]; then
-    git clone https://github.com/AvengerEug/simple-factory.git -b $BRANCH ./design/simple-factory
-  fi
-}
-
-clone_observer() {
-  echo -e "\nClone observer"
-  echo "----------------------------------------------"
-  if [ ! -e ./design/observer ]; then
-    git clone https://github.com/AvengerEug/observer.git -b $BRANCH ./design/observer
-  fi
-}
-
-clone_template-method() {
-  echo -e "\nClone template-method"
-  echo "----------------------------------------------"
-  if [ ! -e ./design/template-method ]; then
-    git clone https://github.com/AvengerEug/template-method.git -b $BRANCH ./design/template-method
-  fi
-}
-
-clone_jwt-token() {
-  echo -e "\nClone jwt-token"
-  echo "----------------------------------------------"
-  if [ ! -e ./javaee/jwt-token ]; then
-    git clone https://github.com/AvengerEug/jwt-token.git -b $BRANCH ./javaee/jwt-token
-  fi
-}
-
-clone_rabbit-mq() {
-  echo -e "\nClone rabbit-mq"
-  echo "----------------------------------------------"
-  if [ ! -e ./rabbitmq ]; then
-    git clone https://github.com/AvengerEug/rabbit-mq.git -b $BRANCH ./rabbitmq/rabbit-mq
-  fi
-}
-
-clone_spring() {
-  echo -e "\nClone spring"
-  echo "----------------------------------------------"
-  if [ ! -e ./basic/spring ]; then
-    git clone https://github.com/AvengerEug/spring.git -b $BRANCH ./basic/spring
   fi
 }
 
